f0573998db9175864bfc820411164c022536ae17
mouadedd <mdx.edd@gmail.com>
2023-10-03 07:26:32 -0700

    variable

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for l in range(97, 123):
-    if chr(l) is not 'q' and chr(l) is not 'e':
-        print("{}".format(chr(l)), end="")
+for lett in range(97, 123):
+    if chr(lett) is not 'q' and chr(lett) is not 'e':
+        print("{}".format(chr(lett)), end="")
