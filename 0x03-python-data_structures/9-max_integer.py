d55edd69df013c4bafb339eb30a42377759f4346
mouadedd <mdx.edd@gmail.com>
2023-10-09 01:26:39 -0700

    find the biggest integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == "":
+        return None
+
+    biggest = my_list[0]
+    for x in range(len(my_list)):
+        if my_list[x] > biggest:
+            biggest = my_list[x]
+    return biggest
