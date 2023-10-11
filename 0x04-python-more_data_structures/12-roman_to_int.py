4532723040dc1c4998e281c88573b301c0b1b8cb
mouadedd <mdx.edd@gmail.com>
2023-10-11 03:17:25 -0700

    roman dic

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,12 @@
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10,
-            'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    decs = [roman_dict[x] for x in roman_string]
+    roman_dict = {'I': 1, 'V': 5, 'X': 10,'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
+    decs = [roman_dict[i] for i in roman_string]
     res = 0
-    for i in range(len(decs)):
-        res += decs[i]
-        if decs[i - 1] < decs[i] and i != 0:
-            res -= (decs[i - 1] + decs[i - 1])
+    for e in range(len(decs)):
+        res += decs[e]
+        if decs[e - 1] < decs[e] and e != 0:
+            res -= (decs[e - 1] + decs[e - 1])
     return res
