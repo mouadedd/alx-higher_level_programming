776c7345e51669233dfab5439d0573aeeb6480ce
mouadedd <mdx.edd@gmail.com>
2023-10-24 06:38:42 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''define a square class'''
+
+
+class Square:
+    '''Represents a square'''
+    def __init__(self, size):
+        '''Initializes a square'''
+        self.__size = size
