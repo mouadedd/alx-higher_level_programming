340a6dbd2ae32c281fbe044c83f09e69e648c2c1
mouadedd <mdx.edd@gmail.com>
2023-11-07 03:03:24 -0800

    pycodestyle tested

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,14 +3,13 @@
 from sys import argv
 
 
-if __name__ == "__main__":
-    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-    try:
-        itm = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        itm = []
-    for ar in argv[1:]:
-        itm.append(ar)
-    save_to_json_file(itm, "add_item.json")
+try:
+    itm = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    itm = []
+for ar in argv[1:]:
+    itm.append(ar)
+save_to_json_file(itm, "add_item.json")
