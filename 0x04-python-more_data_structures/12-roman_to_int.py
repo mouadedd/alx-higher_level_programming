1b8a8748cf7abbbe6e98b73a2ba1288c79954f78
mouadedd <mdx.edd@gmail.com>
2023-12-03 03:47:28 -0800

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) is not str:
-        return 0
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                   'C': 100, 'D': 500, 'M': 1000}
-    decs = [roman_dict[i] for i in roman_string]
-    res = int()
-    for e in range(len(decs)):
-        res += decs[e]
-        if decs[e - 1] < decs[e] and e != 0:
-            res -= 2*(decs[e - 1])
-    return res
+    nums = []
+    chif = int()
+
+    if not roman_string or type(roman_string) is not str:
+        return 0
+    else:
+        for i in roman_string:
+            nums.append(roman_dict[i])
+
+        for n in nums:
+            if len(nums) == 1:
+                chif = n
+            elif nums[-1] < nums[0]:
+                chif += n
+            elif nums[-1] > nums[0]:
+                s = sum(nums[:-1])
+                chif = nums[-1] - s
+        return chif
