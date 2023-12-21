5a9ec648b318ffb13115f9c4a1504e3f61a660f4
mouadedd <mdx.edd@gmail.com>
2023-12-21 06:57:04 -0800

    form changed

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,6 @@
---ists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+--ists all shows contained in hbtn_0d
 SELECT s.`title`, g.`genre_id`
-FROM `tv_shows` AS s
-INNER JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id` ORDER BY s.`title`, g.`genre_id`;
+  FROM `tv_shows` AS s
+        INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
