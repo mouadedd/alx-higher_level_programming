122332009bd2cad2505eddb538f3fbf6dfc53922
mouadedd <mdx.edd@gmail.com>
2023-12-25 04:01:00 -0800

    test

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,5 @@
---creates the table force_name on your MySQL server
-CREATE TABLE IF NOT EXISTS `force_name` (`id` INT, `name` VARCHAR(256) NOT NULL);
+-- Creates table
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id`   INT,
+    `name` VARCHAR(256) NOT NULL
+);
