ab8f5bc493b4efde022e95ae9e39b59058865e94
mouadedd <mdx.edd@gmail.com>
2023-12-25 03:37:09 -0800

    test

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
---lists all cities contained in the database hbtn_0d_usa
+-- Lists all cities in the database hbtn_0d_usa.
 SELECT c.`id`, c.`name`, s.`name`
   FROM `cities` AS c
        INNER JOIN `states` AS s
