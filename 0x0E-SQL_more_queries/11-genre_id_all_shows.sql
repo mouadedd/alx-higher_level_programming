7c06eb4c1f951340f977aae48f57d3c7f1a91659
mouadedd <mdx.edd@gmail.com>
2023-12-25 03:46:10 -0800

    test

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,7 @@
---lists all shows contained in the database hbtn_0d_tvshows
+--lists all shows contained in the database hbtn_0d_tvs
+-- Records are ordered by ascending tv_shows.title
 SELECT s.`title`, g.`genre_id`
-  FROM `tv_shows` AS s LEFT JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id` ORDER BY s.`title`, g.`genre_id`;
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
