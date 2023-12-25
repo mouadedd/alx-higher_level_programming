114511f89535ce43ecd665187f1c0191a1718450
mouadedd <mdx.edd@gmail.com>
2023-12-25 03:39:41 -0800

    test

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
         INNER JOIN `tv_show_genres` AS g
-	ON s.`id` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`;
+        ON s.`id` = g.`show_id`
+  ORDER BY s.`title`, g.`genre_id`;
