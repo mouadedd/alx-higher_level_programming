54cda4c0ec4eaff9e07f7b8f383290baf1c66ec6
mouadedd <mdx.edd@gmail.com>
2024-01-10 05:10:25 -0800

    changed the sort withe the arrow function to sort numbers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 let secBigg = 0;
 const args = process.argv.slice(2);
 if (args.length > 1) {
-  args.sort((a, b)=> a - b);
+  args.sort((a, b) => a - b);
   secBigg = args[args.length - 2];
 }
 console.log(secBigg);
