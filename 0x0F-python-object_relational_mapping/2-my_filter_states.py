30d99b7a1077f65e2af1d3cdf7d3a081c75624fe
mouadedd <mdx.edd@gmail.com>
2024-01-14 02:16:41 -0800

    added order

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
                          db=argv[3], port=3306)
     curs = db.cursor()
-    curs.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+    curs.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id"
                  .format(argv[4]))
     rows = curs.fetchall()
     for row in rows:
