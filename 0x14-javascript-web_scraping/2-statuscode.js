68a170469c2b5d3025a324110e8538d210e565f5
mouadedd <mdx.edd@gmail.com>
2024-02-20 04:07:05 -0800

    tested

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (err, resp) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('code: ', resp.statusCode);
+});
