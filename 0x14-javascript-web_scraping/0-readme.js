b05dbbecbfa795e7438322e1fd7bbc0ca7a2bc14
mouadedd <mdx.edd@gmail.com>
2024-02-20 03:37:25 -0800

    tested

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const fileName = process.argv[2];
+
+fs.readFile(fileName, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
