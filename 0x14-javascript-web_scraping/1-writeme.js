68eee1dd98ceed64a25fba4141c73f0b23f2bb68
mouadedd <mdx.edd@gmail.com>
2024-02-20 03:51:51 -0800

    tested

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const name = process.argv[2];
+const text = process.argv[3];
+
+fs.writeFile(name, text, err => {
+  if (err) {
+    console.error(err);
+  }
+});
